bd0fcb5bedc63928d94b71b26ecaa3a222f6aa86
Xasbus <davidbilliot0828.gmail.com>
2024-09-06 17:35:15 +0000

    ⚾️⛷ Checkpoint ./junkfile.html:69253218/44 ./utility-functions.js:69253218/258

--- utility-functions.js
+++ utility-functions.js
@@ -1,6 +1,18 @@
 function display(message) {
   document.write(message);
 }
+
 function output(message) {
   window.outputTag.innerHTML = window.outputTag.innerHTML + message
 }
+
+function ask(message) {
+   const userText = prompt(message);
+   const userNumber = Number(userText);
+   const isNotNumber = isNaN(userNumber);
+        if (isNotNumber) {
+          return userText;
+        } else {
+          return userNumber;
+        }
+ }
