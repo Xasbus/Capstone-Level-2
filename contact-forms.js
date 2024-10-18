00f3741d650f35557e19fdd031686646d1f34a15
Xasbus <davidbilliot0828.gmail.com>
2024-10-18 12:27:26 -0500

    heading added

--- contact-forms.js
+++ contact-forms.js
@@ -1,7 +1,16 @@
 function numberProcess(event) {
     debugger;
     event.preventDefault();
-    const phoneForm = event.target
+    const phoneForm = event.target;
     const phoneInput = phoneForm[0];
-    const phoneValue = myPasswordInputInput.value;
+    const phoneValue = phoneInput.value;
+    output(` Your phone number is recorded. Please choose a date and time.<br>`)
+}
+
+function dateProcess(event) {
+    event.preventDefault();
+    const dateForm = event.target;
+    const dateInput = dateForm[0];
+    const dateValue = dateInput.value;
+    output(` We have received your appointment. See you there! <br>`)
 }
