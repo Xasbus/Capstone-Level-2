bd0f9ee700806fefdea5311b4e699e03aa5b57b7
Xasbus <davidbilliot0828.gmail.com>
2024-10-18 11:47:56 -0500

    add process_beginning

---
+++ contact-forms.js
@@ -0,0 +1,7 @@
+function numberProcess(event) {
+    debugger;
+    event.preventDefault();
+    const phoneForm = event.target
+    const phoneInput = phoneForm[0];
+    const phoneValue = myPasswordInputInput.value;
+}
