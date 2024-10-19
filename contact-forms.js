d558c83774e8d862e664b63e223e420aee329f66
Xasbus <davidbilliot0828.gmail.com>
2024-10-19 08:21:07 -0500

    Fixed Process

--- contact-forms.js
+++ contact-forms.js
@@ -1,7 +1,13 @@
 function emailProcess(event) {
+    console.log("test");
     event.preventDefault();
     const form = event.target;
     const passwordInput = form[0];
     const value = passwordInput.value;
+    console.log("test1", form);
     output(` <br>We received your info.`);
+}
+
+function output(message) {
+    window.outputTag.innerHTML += message;
 }
